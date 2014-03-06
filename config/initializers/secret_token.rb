b780e2f0833af08d27bbf53ef80baaae6e757c8c
Lorenzo Tello <ltello8a@gmail.com>
2014-03-06 01:35:17 +0100

    rails new authorizator -B -T -d mysql and better .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Authorizator::Application.config.secret_key_base = '4d42328567ed85fc289d9d6e693d71f0a8bd75daa8ff25da5e735674741099ebb54d8d8fa1c59de5f0d79c8e8c9121410ca4d8870baad02d5407d81ee96fcb71'
