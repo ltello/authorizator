d16d6e7d3aa0ae6842e168e3dabef4e612c98df3
Lorenzo Tello <ltello8a@gmail.com>
2014-03-07 17:11:14 +0100

    use scopes to differenciate access_tokens granted to service mates, own service or eventually public api consumers

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -39,6 +39,8 @@ Doorkeeper.configure do
   # For more information go to https://github.com/applicake/doorkeeper/wiki/Using-Scopes
   # default_scopes  :public
   # optional_scopes :write, :update
+  default_scopes  :public
+  optional_scopes :myself, :service_mate
 
   # Change the way client credentials are retrieved from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
