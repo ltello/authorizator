10b5c6a6f266b05f9454d843e753c6e6bd46e711
Lorenzo Tello <ltello8a@gmail.com>
2014-03-07 20:10:50 +0100

    secure client applications management

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -5,7 +5,7 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    OpenStruct.new(:id => 123)
+    current_user or authenticate_user!
     # raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # Put your resource owner authentication logic here.
     # Example implementation:
@@ -14,6 +14,7 @@ Doorkeeper.configure do
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
   admin_authenticator do
+    current_user or authenticate_user!
     # Put your admin authentication logic here.
     # Example implementation:
     # Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
