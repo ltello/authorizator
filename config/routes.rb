e49c6490539b31b2c0e8e529cc91f85ecbfb7713
Lorenzo Tello <ltello8a@gmail.com>
2014-03-07 18:44:32 +0100

    scaffold for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Authorizator::Application.routes.draw do
   devise_for :users
   use_doorkeeper
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'doorkeeper/applications#index'
+
+  resources :users, :except => [:create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
