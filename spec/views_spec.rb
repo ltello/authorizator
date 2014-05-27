fc91d7667a8acd56101bc840af460beaf7a8dc8c
Lorenzo Tello <ltello8a@gmail.com>
2014-05-27 02:18:04 +0200

    fix tests to rspec 3.0.0.rc1

--- spec/views_spec.rb
+++ spec/views_spec.rb
@@ -3,7 +3,7 @@ require 'views/users_views_spec'
 require 'views/services_views_spec'
 
 
-describe "Administrator's HTML/JSON Views:" do
+describe "Administrator's HTML/JSON Views:", type: :view do
 
   include_context 'users views'
   include_context 'services views'
