786772145c1d9a544afd7d45ecf889422423211b
Lorenzo Tello <ltello8a@gmail.com>
2014-06-03 16:12:35 +0200

    remove old code

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -3,29 +3,6 @@ shared_context 'services controller' do
   describe ServicesController do
 
     context "provides data needed by every service in the suite (services names and hosts, talking token...)" do
-      # create_sample_services
-      # create_valid_access_token
-      # create_valid_talking_token
-#
-      # it "/services: (GET) returns a list with data for every service in the suite secured by the Authorizator." do
-      #   # check_routes_for(services_path, verbs: :get)
-      #   get services_path
-      #   expect([401]).to include(response.status)
-      #   get services_path, {}, authorization_header_for_access_token
-      #   expect(response.status).to eq(200)
-      #   expect(json).to eq(sample_services_data)
-      # end
-#
-      # it "/services/talking_token: (GET) returns data of the currently valid service suite talking token." do
-      #   # check_routes_for(talking_token_services_path, verbs: :get)
-      #   get talking_token_services_path
-      #   expect([401]).to include(response.status)
-      #   get talking_token_services_path, {}, authorization_header_for_access_token
-      #   expect(response.status).to eq(200)
-      #   expect(json.except('expires_in')).to eq(valid_talking_token_data.except('expires_in'))
-      #   expect(valid_talking_token_data['expires_in']).to be_within(60).of(json['expires_in'])
-      # end
-#
   ##
    ##     describe "GET index" do
    ##       it "forces the user to be logged in and" do
