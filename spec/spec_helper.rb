bb806d958f15fe2c0f697069e7bb77cdf7a85890
Lorenzo Tello <ltello8a@gmail.com>
2014-06-11 20:44:04 +0200

    run coverage tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,9 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 # require 'rspec/autorun'
 
+require 'simplecov'
+SimpleCov.start 'rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
